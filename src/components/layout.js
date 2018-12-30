5ce83a897e710956aafb4b381422db7ead7b180c
Simon vom Eyser <simon.vom.eyser@gmail.com>
2018-12-30 14:14:14 +0100

    Add start of basic l10n solution

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,56 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import LangSwitch from '../components/langSwitch';
-import { StaticQuery, graphql } from 'gatsby'
+import { injectIntl } from 'react-intl'
 
 import './layout.css'
 import { Link } from '@reach/router';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <div
-          style={{
-            margin: '60px auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          <h1>
-            <Link to="/">Simon vom Eyser</Link> 
-          </h1>
-          <LangSwitch />
-          <br/>
-          <br/>
-          {children}
-        </div>
-      </>
-    )}
-  />
+const Layout = ({ children, data, intl }) => (
+  <div>
+    <Helmet
+      title={intl.formatMessage({ id: 'title' })}
+      meta={[
+        { name: 'description', content: intl.formatMessage({ id: 'welcome' }) },
+        {
+          name: 'keywords',
+          content: 'gatsby, i18n, react-intl, multi language, localization',
+        },
+      ]}
+    >
+      <html lang="en" />
+    </Helmet>
+    <div
+      style={{
+        margin: '60px auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      <h1>
+        <Link to="/">Simon vom Eyser</Link> 
+      </h1>
+      <h2>
+        Webentwicklung
+      </h2>
+      <LangSwitch />
+      <br/>
+      <br/>
+      {children}
+    </div>
+  </div>
 )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default injectIntl(Layout);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { withIntl, Link } from '../i18n'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
     <h2>Hello</h2>
-    <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/projects/">Show Projects</Link><br/>
     <Link to="/impressum/">Show Imprint</Link>
   </Layout>
 )
 
-export default IndexPage
+export default withIntl(IndexPage);
