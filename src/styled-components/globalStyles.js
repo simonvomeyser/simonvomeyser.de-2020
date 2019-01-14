03b00998128738d6daffb46ef3b0ead1202d200e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-14 10:06:22 +0100

    Add first global styles

--- src/styled-components/globalStyles.js
+++ src/styled-components/globalStyles.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
+import {vars} from '../util/vars'
 
 export default createGlobalStyle`
     body {
         ${reset}
-        background-color: green;
+        margin: 0;
+        background-color: ${vars.styles.colors.white3};
     }
 `;
