05e26e69123a919ca852d3cea70c02848d3a5877
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-14 10:01:31 +0100

    Add variables file

---
+++ src/util/variables.js
@@ -0,0 +1,14 @@
+export const vars = {
+    styles: {
+        colors: {
+            white1: '#FFF',
+            white2: '#F7F7F7',
+            white3: '#EEEFF1',
+            green1: '#6EB9B3',
+            lightGrey1:  '#999DA1',
+            lightGrey2:  '#DDDDDD',
+            grey1: '#3F474F',
+            grey2: '#60676C'
+        }
+    }
+}
