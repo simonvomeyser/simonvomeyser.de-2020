b5619b1ac2502118b441ea41c70a7c62227a61e3
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-16 07:41:32 +0100

    Show logo in navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import styled  from 'styled-components';
 import {vars} from '../util/vars'
+import Link  from '../i18n/Link'
+import Logo from '../images/logo-navigation.svg'
 
 export default class Navigation extends Component {
   render() {
     return (
       <Wrapper>
-        <nav>nav</nav>
+        <NavigationTop>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">
+                  <img src={Logo} alt=""/>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  About me
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </NavigationTop>
       </Wrapper>
     )
   }
@@ -19,4 +51,21 @@ const Wrapper = styled.div`
   top: 0;
   width: ${vars.styles.sizes.navigationWidth};
   height: 100%;
+`
+
+const NavigationTop = styled.div`
+  padding-top: 1rem;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  img {
+    max-width: 60%;
+    height: auto;
+  }
+  li {
+    text-align: center;
+  }
 `
