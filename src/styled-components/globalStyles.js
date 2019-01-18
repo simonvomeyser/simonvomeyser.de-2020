ddbc164b0b2024b86d57217c292cac03c26cbdf0
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-18 07:10:54 +0100

    Fix global styles, add big base font size

--- src/styled-components/globalStyles.js
+++ src/styled-components/globalStyles.js
@@ -3,9 +3,17 @@ import reset from 'styled-reset'
 import {vars} from '../util/vars'
 
 export default createGlobalStyle`
+    ${reset}
+    html {
+        font-size: 20px;
+        font-family: sans-serif;
+    }
     body {
-        ${reset}
         margin: 0;
         background-color: ${vars.styles.colors.white3};
+    }
+    a, a:hover, a:focus, a:visited, a:active {
+        text-decoration: none;
+        color: inherit;
     }
 `;
