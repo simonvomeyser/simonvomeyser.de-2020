4692126099bc48ebfb2608736bbdee0cc745a664
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-20 08:03:46 +0100

    Get basic frontpage layout to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import { withIntl } from '../i18n'
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl'
 import Layout from '../components/layout'
+import styled from 'styled-components'
+import { vars } from '../util/vars'
+import LogoIcon from '../svg/logo.svg'
 
 const IndexPage = () => (
   <Layout>
-    <h1> <FormattedMessage id="landingTitle1"/> </h1>
-    <h2> <FormattedMessage id="landingTitle2"/> </h2>
+    <Wrapper>
+      <LogoWrapper>
+        <LogoIcon />
+      </LogoWrapper>
+      <SuperHeading>
+        {' '}
+        <FormattedMessage id="landingTitle2" />{' '}
+      </SuperHeading>
+      <Heading>
+        {' '}
+        <FormattedMessage id="landingTitle1" />{' '}
+      </Heading>
+    </Wrapper>
   </Layout>
 )
 
-export default withIntl(IndexPage);
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+`
+
+const LogoWrapper = styled.div`
+  svg {
+    width: 30vw;
+  }
+  margin-bottom: 2rem;
+`
+
+const Heading = styled.h1`
+  font-size: 4rem;
+  margin-bottom: 1rem;
+  color: ${vars.styles.colors.grey1};
+  text-align: center;
+`
+
+const SuperHeading = styled.div`
+  font-size: 2rem;
+  color: ${vars.styles.colors.grey3};
+  text-align: center;
+`
+
+export default withIntl(IndexPage)
