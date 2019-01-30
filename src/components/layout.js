2bf7c286dda1cbaaaed3f28eedf55c28f5b5b223
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-01-30 06:55:24 +0100

    Format and refactor

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,34 +2,35 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { injectIntl } from 'react-intl'
-import Navigation from '../components/navigation';
+import Navigation from '../components/navigation'
 import GlobalStyles from '../styled-components/globalStyles'
-import styled  from 'styled-components';
-import {vars} from '../util/vars'
+import styled from 'styled-components'
+import { vars } from '../util/vars'
 
 const Layout = ({ children, data, intl }) => (
   <Fragment>
-    <GlobalStyles/>
+    <GlobalStyles />
     <Helmet
       title={intl.formatMessage({ id: 'title' })}
       meta={[
-        { name: 'description', content: intl.formatMessage({ id: 'description' }) },
+        {
+          name: 'description',
+          content: intl.formatMessage({ id: 'description' }),
+        },
         {
           name: 'keywords',
-          content: intl.formatMessage({ id: 'keywords' })
-        }
+          content: intl.formatMessage({ id: 'keywords' }),
+        },
       ]}
     >
       <html lang="en" />
     </Helmet>
-    <Navigation/>
-    <Wrapper>
-      {children}
-    </Wrapper>
+    <Navigation />
+    <StyledWrapper>{children}</StyledWrapper>
   </Fragment>
 )
 
-const Wrapper = styled.div`
+const StyledWrapper = styled.div`
   padding-left: ${vars.styles.sizes.navigationWidth};
 `
 
@@ -37,4 +38,4 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default injectIntl(Layout);
+export default injectIntl(Layout)
