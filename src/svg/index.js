f5f85fcfddc6827df6b649863b4ee216d9ba3acd
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-02-03 17:06:21 +0100

    Add basic burger

--- src/svg/index.js
+++ src/svg/index.js
@@ -4,6 +4,7 @@ import Projects from 'svg/projects.svg'
 import Write from 'svg/write.svg'
 import PaperPlane from 'svg/paper-plane.svg'
 import Legal from 'svg/legal.svg'
+import Burger from 'svg/burger.svg'
 
 export const LogoNavigationSvg = LogoNavigation
 export const UserSvg = User
@@ -11,3 +12,4 @@ export const ProjectsSvg = Projects
 export const WriteSvg = Write
 export const PaperPlaneSvg = PaperPlane
 export const LegalSvg = Legal
+export const BurgerSvg = Burger
