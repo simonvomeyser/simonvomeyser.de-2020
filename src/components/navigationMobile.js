4231802fa42d46493a548e30add618fbac974d82
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-02-03 16:35:47 +0100

    Perfectly center logo independent from burger and lang

--- src/components/navigationMobile.js
+++ src/components/navigationMobile.js
@@ -27,6 +27,10 @@ export default class NavigationMobile extends Component {
 }
 
 const LogoWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   svg {
     height: 40px;
     width: auto;
