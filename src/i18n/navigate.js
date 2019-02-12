3ce01849dea28b53a2518bba279f493db3c6577e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-02-12 07:14:40 +0100

    Write basic navigate function with lang

---
+++ src/i18n/navigate.js
@@ -0,0 +1,19 @@
+import { withPrefix } from 'gatsby-link'
+import browserLang from 'browser-lang'
+import { languages } from './index'
+import { navigate as gatsbyNavigate } from 'gatsby'
+
+export const navigate = (location = '/') => {
+  const langKeys = languages.map(language => language.value)
+
+  const detected =
+    window.localStorage.getItem('language') ||
+    browserLang({
+      languages: langKeys,
+      fallback: 'en',
+    })
+
+  const newUrl = withPrefix(`/${detected}${location}`)
+
+  gatsbyNavigate(newUrl)
+}
