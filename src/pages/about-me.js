425f03df16134af49e7b2365b5682e75e6a0d4ef
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-02-14 07:43:08 +0100

    Add sample content to about me

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -8,7 +8,22 @@ import PageLayout from 'components/pageLayout'
 
 const IndexPage = () => (
   <Layout>
-    <PageLayout>about me</PageLayout>
+    <PageLayout>
+      <h1>About me</h1>
+      <p>Per platea est ut nibh a habitant</p>
+      <p>
+        Mattis rutrum nostra curae scelerisque varius purus quam taciti risus
+        aenean tristique praesent quisque placerat, bibendum velit primis
+        euismod lacus integer mauris dolor ornare mollis ad pellentesque cras.
+        Cras mauris platea massa eu magna tellus venenatis nam molestie bibendum
+        lorem facilisis, auctor potenti luctus viverra congue mus erat inceptos
+        dolor lectus ornare nec, tincidunt varius vestibulum ridiculus purus
+        parturient vel dis orci sagittis aliquet. Ridiculus pretium lectus
+        consectetur dis inceptos quisque eleifend eu imperdiet, hendrerit semper
+        praesent gravida viverra blandit dolor magna, diam euismod sapien luctus
+        torquent lorem mollis tincidunt.
+      </p>
+    </PageLayout>
   </Layout>
 )
 
