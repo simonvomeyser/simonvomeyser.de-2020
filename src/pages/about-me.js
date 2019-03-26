a7bd13fb5a939602cfb24106a7eeda028d2376bb
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-03-26 11:04:17 +0100

    Add fading in image

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -10,125 +10,70 @@ import { FormattedMessage, FormattedHTMLMessage } from 'react-intl'
 import { on } from 'util/breakpoint'
 import ChangeTitle from '../components/ChangeTitle'
 
-const AboutMePage = () => (
+import Img from 'gatsby-image'
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "about-me-1.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 700) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+const AboutMePage = ({ data }) => (
   <Layout>
     <ChangeTitle additionalText="navigationAboutMe" />
     <PageLayout>
       <StyledBackgroundWrapper>
         <StyledBackgroundWrapper.Image> </StyledBackgroundWrapper.Image>
-        <StyledBackgroundWrapper.Content>
+        <StyledBackgroundWrapper.Left>
           <StyledPageHeading>
-            <StyledSpeachBubble>Heyo!</StyledSpeachBubble>
             <FormattedMessage id="niceToMeetYou" />
           </StyledPageHeading>
-          <StyledIntro>
-            <StyledIntro.Image>
-              <PolaroidStack
-                images={[
-                  'https://res.cloudinary.com/simonvomeyser/image/upload/v1551546212/simonvomeyser.de/about-me-1.png',
-                  'https://res.cloudinary.com/simonvomeyser/image/upload/v1551691377/simonvomeyser.de/about-me-2.jpg',
-                ]}
-              />
-            </StyledIntro.Image>
-            <StyledIntro.Text>
-              <StyledSubHeading>
-                <FormattedHTMLMessage id="aboutMeOpener" />
-              </StyledSubHeading>
-              <p>
-                Etwas genauer genommen plane, leite und entwickele ich seit 8
-                Jahren Webanwendungen mit JavaScript und PHP .. und das echt mit
-                viel Spaß.
-              </p>
-
-              <p>
-                Ich bin davon überzeugt, dass man nur wirklich gut in Dingen
-                ist, die man gerne tut.
-              </p>
-            </StyledIntro.Text>
-          </StyledIntro>
-          <StyledSubHeading>Und sonst so?</StyledSubHeading>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-            hic reprehenderit autem, ipsa, amet consequatur, facere molestias
-            nulla alias quidem id labore sequi! Quae explicabo ullam voluptatum
-            quia fugit fuga.
-          </p>
-        </StyledBackgroundWrapper.Content>
+        </StyledBackgroundWrapper.Left>
+        <StyledBackgroundWrapper.Right>
+          <Img fluid={data.file.childImageSharp.fluid} />
+        </StyledBackgroundWrapper.Right>
       </StyledBackgroundWrapper>
     </PageLayout>
   </Layout>
 )
 export default withIntl(AboutMePage)
 
-const StyledSubHeading = styled.h2`
-  color: ${vars.styles.colors.neutral6};
-  font-size: ${vars.styles.fontSizes.size5};
-  line-height: 1.2;
-  font-weight: 600;
-  margin-bottom: 1rem;
-`
-
 const StyledPageHeading = styled.h1`
   font-size: ${vars.styles.fontSizes.size7};
   font-family: ${vars.styles.fontFamilies.special};
-  display: flex;
-  justify-content: center;
   color: ${vars.styles.colors.accent4};
-  align-items: center;
   margin-bottom: 3rem;
-`
-
-const StyledSpeachBubble = styled.span`
-  color: ${vars.styles.colors.white};
-  font-family: ${vars.styles.fontFamilies.normal};
-  position: relative;
-  padding: 0.5rem 1.2rem;
-  background-color: ${vars.styles.colors.accent4};
-  display: inline-block;
-  margin-right: 1rem;
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: -20px;
-    left: 20%;
-    border-top: 20px solid ${vars.styles.colors.accent4};
-    border-right: 20px solid transparent;
-    width: 0;
-    height: 0;
-  }
-  ${on('onlyMobile')} {
-    display: none;
-  }
-`
-
-const StyledIntro = styled.div`
-  display: flex;
-  ${on('onlyMobile')} {
-    display: block;
-  }
-`
-
-StyledIntro.Image = styled.div`
-  padding-right: 1rem;
-  min-height: 350px;
-  flex: 1;
-`
-
-StyledIntro.Text = styled.div`
-  flex: 1;
+  text-align: center;
 `
 
 const StyledBackgroundWrapper = styled.div`
   position: relative;
+  margin-top: 3rem;
   z-index: ${vars.styles.zIndices.base};
+  display: flex;
 `
 
 StyledBackgroundWrapper.Image = styled(AboutMeBackgroundSvg)`
   position: absolute;
-  top: -1rem;
+  top: -3rem;
   z-index: -1;
   left: 0;
 `
-StyledBackgroundWrapper.Content = styled.div`
+
+StyledBackgroundWrapper.Left = styled.div`
   z-index: 1;
+  width: 60%;
+  padding-right: 2rem;
+`
+StyledBackgroundWrapper.Right = styled.div`
+  z-index: 1;
+  width: 40%;
 `
