f9e30cefcc6fb1ef92cd00d53bfff1bf7fa4ccef
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-04-01 14:31:11 +0200

    Add mobile styles and fixed height

--- src/styled-components/buttons.js
+++ src/styled-components/buttons.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { vars } from '../util/vars'
 import Link from 'i18n/Link'
+import { on } from 'util/breakpoint'
 
 const buttonStyles = `
   font-size: ${vars.styles.fontSizes.size6};
@@ -8,10 +9,14 @@ const buttonStyles = `
   border: 3px solid ${vars.styles.colors.accent4};
   font-weight: 500;
   padding: 8px 5px;
+  height: 54px;
   min-width: 200px;
   transition: all 0.3s ease-in-out;
   :hover {
     opacity: 0.8;
+  }
+  ${on('onlyMobile')} {
+      width: 100%;
   }
 `
 export const StyledPrimaryButton = styled.button`
