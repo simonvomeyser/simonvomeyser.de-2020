0cdd4b1ddc784006a07af80d80cbd0c59d17c021
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-04-02 12:09:33 +0200

    Improve button styles

--- src/styled-components/buttons.js
+++ src/styled-components/buttons.js
@@ -4,16 +4,27 @@ import Link from 'i18n/Link'
 import { on } from 'util/breakpoint'
 
 const buttonStyles = `
-  font-size: ${vars.styles.fontSizes.size6};
+  font-size: ${vars.styles.fontSizes.size5};
   font-family: ${vars.styles.fontFamilies.special};
   border: 3px solid ${vars.styles.colors.accent4};
   font-weight: 500;
-  padding: 8px 5px;
-  height: 54px;
-  min-width: 200px;
-  transition: all 0.3s ease-in-out;
-  :hover {
-    opacity: 0.8;
+  padding: 7px 20px;
+  height: 48px;
+  min-width: 180px;
+  transition: all 0.28s cubic-bezier(0.4, 0, 0.2, 1);
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.07), 
+    0 1px 5px 0 rgba(0, 0, 0, 0.06), 
+    0 3px 1px -2px rgba(0, 0, 0, 0.1);
+
+  &:active {
+  box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
+        0 3px 14px 2px rgba(0, 0, 0, 0.12),
+        0 5px 5px -3px rgba(0, 0, 0, 0.4);
+  }
+  outline: none;
+  :hover, :focus {
+    opacity: 0.9;
+    transform: scale(1.01);
   }
   ${on('onlyMobile')} {
       width: 100%;
