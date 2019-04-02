776d0998dc72396cf2a4ea1eff35ecf90e76d624
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-04-02 11:59:15 +0200

    Simplify naming

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,25 +1,25 @@
-import LogoNavigation from 'svg/logo-navigation.svg'
-import User from 'svg/user.svg'
-import Projects from 'svg/projects.svg'
-import Write from 'svg/write.svg'
-import PaperPlane from 'svg/paper-plane.svg'
-import Legal from 'svg/legal.svg'
-import Burger from 'svg/burger.svg'
-import UnitedStates from 'svg/united-states.svg'
-import Germany from 'svg/germany.svg'
+import logoNavigation from 'svg/logo-navigation.svg'
+import user from 'svg/user.svg'
+import projects from 'svg/projects.svg'
+import write from 'svg/write.svg'
+import paperplane from 'svg/paper-plane.svg'
+import legal from 'svg/legal.svg'
+import burger from 'svg/burger.svg'
+import unitedstates from 'svg/united-states.svg'
+import germany from 'svg/germany.svg'
 import xing from 'svg/xing.svg'
 import mail from 'svg/mail.svg'
 import twitter from 'svg/twitter.svg'
 
-export const LogoNavigationSvg = LogoNavigation
-export const UserSvg = User
-export const ProjectsSvg = Projects
-export const WriteSvg = Write
-export const PaperPlaneSvg = PaperPlane
-export const LegalSvg = Legal
-export const BurgerSvg = Burger
-export const UnitedStatesSvg = UnitedStates
-export const GermanySvg = Germany
+export const LogoNavigationSvg = logoNavigation
+export const UserSvg = user
+export const ProjectsSvg = projects
+export const WriteSvg = write
+export const PaperPlaneSvg = paperplane
+export const LegalSvg = legal
+export const BurgerSvg = burger
+export const UnitedStatesSvg = unitedstates
+export const GermanySvg = germany
 export const TwitterSvg = twitter
 export const MailSvg = mail
 export const XingSvg = xing
