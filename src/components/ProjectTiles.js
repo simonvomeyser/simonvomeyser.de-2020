06820b30a40c224bf779edc991c808857da18546
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-04-13 13:23:52 +0200

    Extract out tiles component

---
+++ src/components/ProjectTiles.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { vars } from 'util/vars'
+import { FormattedMessage } from 'react-intl'
+import { on } from 'util/breakpoint'
+import { withIntl, Link } from 'i18n'
+
+export default class ProjectTiles extends Component {
+  render() {
+    return (
+      <StyledProjectTiles>
+        {this.props.projects.map(({ node }) => {
+          return (
+            <StyledProjectTiles.Item key={node.frontmatter.key}>
+              <Link to={'/projects/' + node.frontmatter.key}>
+                <img
+                  src={node.frontmatter.logo}
+                  alt={node.frontmatter.name + ' list image'}
+                />
+                <h2>{node.frontmatter.name}</h2>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.excerpt,
+                  }}
+                />
+              </Link>
+            </StyledProjectTiles.Item>
+          )
+        })}
+      </StyledProjectTiles>
+    )
+  }
+}
+
+const StyledProjectTiles = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 1rem;
+`
+
+StyledProjectTiles.Item = styled.li`
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.25);
+  img {
+    margin-bottom: 1rem;
+  }
+  h2 {
+    font-size: ${vars.styles.fontSizes.size6};
+    text-align: center;
+    color: ${vars.styles.colors.neutral3};
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: ${vars.styles.fontSizes.size3};
+    background: ${vars.styles.colors.neutral8};
+    padding: 0.5rem;
+  }
+`
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,7 @@ import ChangeTitle from '../components/ChangeTitle'
 import PageLayout from 'components/PageLayout'
 import { StyledPageHeading } from '../styled-components'
 import styled from 'styled-components'
+import ProjectTiles from '../components/ProjectTiles'
 
 class SecondPage extends React.Component {
   getLocalizedProjects = () => {
@@ -32,26 +33,7 @@ class SecondPage extends React.Component {
               Klick die Projekte an um mehr zu erfahren oder benutz die Suche 🙂
             </p>
           </StyledSubHeadingText>
-          <StyledProjectList>
-            {projects.map(({ node }) => {
-              return (
-                <StyledProjectListItem key={node.frontmatter.key}>
-                  <Link to={'/projects/' + node.frontmatter.key}>
-                    {node.frontmatter.name} (node.frontmatter.excerpt})
-                  </Link>
-                </StyledProjectListItem>
-              )
-            })}
-            {projects.map(({ node }) => {
-              return (
-                <StyledProjectListItem key={node.frontmatter.key}>
-                  <Link to={'/projects/' + node.frontmatter.key}>
-                    {node.frontmatter.name} ({node.frontmatter.excerpt})
-                  </Link>
-                </StyledProjectListItem>
-              )
-            })}
-          </StyledProjectList>
+          <ProjectTiles projects={projects} />
         </PageLayout>
       </Layout>
     )
@@ -60,16 +42,6 @@ class SecondPage extends React.Component {
 
 const StyledSubHeadingText = styled.div`
   margin-bottom: 2rem;
-`
-
-const StyledProjectList = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1rem;
-`
-
-const StyledProjectListItem = styled.li`
-  border: 1px solid grey;
 `
 
 export default withIntl(SecondPage)
