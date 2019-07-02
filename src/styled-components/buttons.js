644517377af78a836519607601d7f1ed14b8f5bf
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-07-02 12:17:34 +0200

    Provide possibility to customize buttons

--- src/styled-components/buttons.js
+++ src/styled-components/buttons.js
@@ -7,18 +7,19 @@ const buttonStyles = `
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: ${vars.styles.fontSizes.size5};
+  font-size: ${props =>
+    props.smaller ? vars.styles.fontSizes.size4 : vars.styles.fontSizes.size5};
   font-family: ${vars.styles.fontFamilies.special};
   border: 3px solid ${vars.styles.colors.accent4};
   font-weight: 500;
   padding: 7px 20px;
-  height: 48px;
+  height: ${props => (props.smaller ? '28px' : '48px')};
   min-width: 180px;
   transition: all 0.28s cubic-bezier(0.4, 0, 0.2, 1);
-  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.07), 
-    0 1px 5px 0 rgba(0, 0, 0, 0.06), 
-    0 3px 1px -2px rgba(0, 0, 0, 0.1);
-
+  box-shadow: ${props =>
+    props.noShadow
+      ? 'none'
+      : '0 2px 2px 0 rgba(0, 0, 0, 0.07), 0 1px 5px 0 rgba(0, 0, 0, 0.06), 0 3px 1px -2px rgba(0, 0, 0, 0.1)'};
   &:active {
   box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
         0 3px 14px 2px rgba(0, 0, 0, 0.12),
