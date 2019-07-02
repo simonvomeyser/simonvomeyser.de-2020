cabf097d4386a323d1fe9513323710a4e02814fe
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-07-02 12:48:51 +0200

    Fix issue with buttons, buttons styles did not get props

--- src/styled-components/buttons.js
+++ src/styled-components/buttons.js
@@ -3,23 +3,17 @@ import { vars } from '../util/vars'
 import Link from 'i18n/Link'
 import { on } from 'util/breakpoint'
 
-const buttonStyles = `
+const buttonStyles = props => `
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: ${props =>
-    props.smaller ? vars.styles.fontSizes.size4 : vars.styles.fontSizes.size5};
   font-family: ${vars.styles.fontFamilies.special};
   border: 3px solid ${vars.styles.colors.accent4};
   font-weight: 500;
   padding: 7px 20px;
-  height: ${props => (props.smaller ? '28px' : '48px')};
   min-width: 180px;
   transition: all 0.28s cubic-bezier(0.4, 0, 0.2, 1);
-  box-shadow: ${props =>
-    props.noShadow
-      ? 'none'
-      : '0 2px 2px 0 rgba(0, 0, 0, 0.07), 0 1px 5px 0 rgba(0, 0, 0, 0.06), 0 3px 1px -2px rgba(0, 0, 0, 0.1)'};
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.07), 0 1px 5px 0 rgba(0, 0, 0, 0.06), 0 3px 1px -2px rgba(0, 0, 0, 0.1);
   &:active {
   box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
         0 3px 14px 2px rgba(0, 0, 0, 0.12),
@@ -34,13 +28,27 @@ const buttonStyles = `
       width: 100%;
   }
 `
+
+const getHeight = props => {
+  return props.smaller ? '36px' : '48px'
+}
+const getFontSize = props => {
+  return props.smaller
+    ? vars.styles.fontSizes.size4
+    : vars.styles.fontSizes.size5
+}
+
 export const StyledPrimaryButton = styled.button`
   ${buttonStyles}
+  height: ${getHeight};
+  font-size: ${getFontSize};
   background-color: ${vars.styles.colors.accent4};
   color: ${vars.styles.colors.white};
 `
 export const StyledSecondaryButton = styled.button`
   ${buttonStyles}
+  height: ${getHeight};
+  font-size: ${getFontSize};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -51,12 +59,16 @@ export const StyledSecondaryButton = styled.button`
 
 export const StyledPrimaryButtonLink = styled(Link)`
   ${buttonStyles}
+  height: ${getHeight};
+  font-size: ${getFontSize};
   background-color: ${vars.styles.colors.accent4};
   color: ${vars.styles.colors.white};
 `
 
 export const StyledSecondaryButtonLink = styled(Link)`
   ${buttonStyles}
+  height: ${getHeight};
+  font-size: ${getFontSize};
   box-shadow: none;
   border: 3px solid ${vars.styles.colors.accent4};
   color: ${vars.styles.colors.accent4};
