9518aa8fb2cc8737bbaed4cbee71f8cae3aa3865
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-07-02 17:46:38 +0200

    Remove mobile go to project button
    This was causing issues with the outer link of the card.
    
    Not worth the effort

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -15,8 +15,10 @@ import {
 export default class ProjectTile extends Component {
   render() {
     const project = this.props.node.frontmatter
+    const projectUrl = '/projects/' + project.key
     return (
-      <StyledWrapper href={'projects/' + project.key}>
+      <StyledWrapper>
+        <StyledLink to={projectUrl}>&nbsp;</StyledLink>
         <StyledPreviewImageWrapper>
           <StyledPreviewImage>
             <Img fluid={this.props.image} />
@@ -26,17 +28,22 @@ export default class ProjectTile extends Component {
           <h2>{project.name}</h2>
           <p dangerouslySetInnerHTML={{ __html: project.excerpt }} />
         </StyledContent>
-        <StyledFooterWrapper>
-          <StyledPrimaryButtonLink smaller noShadow>
-            more info
-          </StyledPrimaryButtonLink>
-        </StyledFooterWrapper>
       </StyledWrapper>
     )
   }
 }
 
-const StyledWrapper = styled.a`
+const StyledLink = styled(Link)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`
+
+const StyledWrapper = styled.div`
+  position: relative;
   box-shadow: 0 4px 6px 1px rgba(0, 0, 0, 0.1);
   background: ${vars.styles.colors.neutral8};
   transition: all 0.3s ease-in-out;
@@ -48,17 +55,6 @@ const StyledWrapper = styled.a`
     box-shadow: 0 5px 8px 1px rgba(0, 0, 0, 0.2);
   }
 `
-const StyledFooterWrapper = styled.div`
-  display: none;
-  ${on('tablet')} {
-    padding: 0.75rem;
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    justify-content: flex-end;
-  }
-`
-
 const StyledPreviewImage = styled.div`
   position: absolute;
   top: 50%;
