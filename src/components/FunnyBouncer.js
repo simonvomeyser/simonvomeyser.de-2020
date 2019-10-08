430a55dee3bf37c728714641d4420d7f56bc7e97
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-10-08 18:18:00 +0200

    Add basic bounce animation

--- src/components/FunnyBouncer.js
+++ src/components/FunnyBouncer.js
@@ -7,10 +7,18 @@ import { ArrowDown } from '../svg'
 import Link from 'i18n/Link'
 
 export default class FunnyBouncer extends Component {
+  constructor(params) {
+    super()
+    this.state = {
+      bounce: 1,
+    }
+  }
   render() {
     return (
       <Wrapper to="/about-me">
-        <ArrowDown />
+        <BounceAnimation>
+          <ArrowDown />
+        </BounceAnimation>
       </Wrapper>
     )
   }
@@ -21,6 +29,7 @@ const Wrapper = styled(Link)`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  transform: translateY(100%);
   p {
     font-size: ${vars.styles.fontSizes.size2};
     margin-bottom: 4px;
@@ -30,5 +39,34 @@ const Wrapper = styled(Link)`
   svg {
     width: 30px;
     height: 30px;
+  }
+`
+
+const BounceAnimation = styled.div`
+  animation: bounce 3s infinite 2s;
+  @keyframes bounce {
+    from,
+    20%,
+    53%,
+    80%,
+    to {
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+      transform: translate3d(0, 0, 0);
+    }
+
+    40%,
+    43% {
+      animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+      transform: translate3d(0, -150%, 0);
+    }
+
+    70% {
+      animation-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+      transform: translate3d(0, -75%, 0);
+    }
+
+    90% {
+      transform: translate3d(0, 0, 0);
+    }
   }
 `
