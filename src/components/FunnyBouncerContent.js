69005e23c7eec3d79bd38bd45cfbd3a2e7060bd2
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-10-15 20:26:27 +0200

    Add example on how funny bouncer would work

--- src/components/FunnyBouncerContent.js
+++ src/components/FunnyBouncerContent.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { vars } from '../util/vars'
-import posed from 'react-pose'
-import { ArrowDown } from '../svg'
-import Link from 'i18n/Link'
+import { FormattedMessage } from 'react-intl'
 
 export default class FunnyBouncerContent extends Component {
   static propTypes = {
     index: PropTypes.number,
   }
-
-  static state = {
-    bla: 'blo',
-  }
-
   render() {
     return (
       <Wrapper to="/about-me">
         <div>
-          <p>Wop wop, come one, click {this.props.index}</p>
-          <p>🙂</p>
+          <FormattedMessage
+            id={'funnyBouncerContent_1_' + (this.props.index % 11) || 1}
+          />
         </div>
       </Wrapper>
     )
@@ -29,4 +22,6 @@ export default class FunnyBouncerContent extends Component {
 
 const Wrapper = styled.div`
   text-align: center;
+  min-height: 2rem;
+  color: black;
 `
