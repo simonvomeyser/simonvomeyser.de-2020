56b470a161eb5ed2a907dadde9dd87db3a8c411f
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-11-18 17:40:26 +0100

    Prevent ssr imports to break a build

--- src/util/isSearchEngineBot.js
+++ src/util/isSearchEngineBot.js
@@ -1,4 +1,8 @@
 export const isSearchEngineBot = () => {
+  if (typeof window === 'undefined') {
+    return false
+  }
+
   return /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(
     navigator.userAgent
   )
