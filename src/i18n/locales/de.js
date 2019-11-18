6bea5c3a1877319be82e23fe6f0b4d0cd456dc7f
Simon vom Eyser <simon.vom.eyser@gmail.com>
2019-11-18 18:16:39 +0100

    Add geman legal notice

--- src/i18n/locales/de.js
+++ src/i18n/locales/de.js
@@ -12,9 +12,46 @@ module.exports = {
   privacyPolicyTitle: 'Datenschutzerklärung',
   privacyPolicyTitleShort: 'Datenschutz',
   imprintContent: `
-  <p>
-  Deutsch imprint
-  </p>
+  <div class="impressum">
+        <p>Angaben gemäß § 5 TMG</p>
+        <p>Simon vom Eyser <br>
+            Ostwall 234<br>
+            47798 Krefeld<br>
+        </p>
+        <h2>Kontakt</h2>
+        <p>
+            Telefon: +49177 4100666<br>
+            E-Mail: <a href="mailto:simon.vom.eyser@gmail.com">simon.vom.eyser@gmail.com</a></p>
+        <h2>Umsatzsteuer-ID:</h2>
+        <p>
+        Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: DE318030105
+        </p>
+        <h2>Haftung für Links</h2>
+        <p>
+            Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
+            Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+            Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+            wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
+            Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist
+            jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+            Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+        </p>
+        <h2>Urheberrecht</h2>
+        <p>
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
+            Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+            Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+            Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit
+            die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
+            beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+            Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
+            von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+        </p><br>
+        <p>
+          Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a>
+            from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        </p>
+    </div>
   `,
   privacyPolicyContent: `
   <p>
@@ -26,7 +63,8 @@ module.exports = {
   navigationProjects: 'Projekte',
   navigationContact: 'Kontakt',
   navigationBlog: 'Blog',
-  navigationLegal: 'Impressum & Datenschutz',
+  navigationImprint: 'Impressum',
+  navigationPrivacyPolicy: '',
   myProfession: 'Webentwicklung',
   niceToMeetYou: 'Cool dass du hier bist! :)',
   aboutMeOpener: 'Ich bin Simon und ich mache <i>irgendwas im Web</i>',
