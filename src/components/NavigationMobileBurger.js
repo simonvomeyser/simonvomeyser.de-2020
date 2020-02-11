f33c534fa1fe24f0fd94690c635b31abf998747d
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-02-11 13:39:26 +0100

    Add fancy burger animation

--- src/components/NavigationMobileBurger.js
+++ src/components/NavigationMobileBurger.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { vars } from '../util/vars'
+import posed from 'react-pose'
 
 export default function NavigationMobileBurger(props) {
   return (
     <StyledWrapper onClick={props.toggleMenu}>
-      <span></span>
-      <span></span>
-      <span></span>
+      <PosedBurgerBarTop pose={props.open ? 'open' : 'closed'} />
+      <PosedBurgerBarCenter pose={props.open ? 'open' : 'closed'} />
+      <PosedBurgerBarBottom pose={props.open ? 'open' : 'closed'} />
     </StyledWrapper>
   )
 }
@@ -30,3 +31,56 @@ const StyledWrapper = styled.button`
     }
   }
 `
+
+const PosedBurgerBarTop = posed('span')({
+  closed: {
+    y: 0,
+    rotate: 0,
+  },
+  open: {
+    y: 8,
+    rotate: 45,
+    transition: {
+      y: {
+        delay: 100,
+      },
+      rotate: {
+        delay: 350,
+        type: 'spring',
+        stiffness: 400,
+        damping: 18,
+      },
+    },
+  },
+})
+
+const PosedBurgerBarCenter = posed('span')({
+  closed: {
+    width: 30,
+  },
+  open: {
+    width: 0,
+  },
+})
+
+const PosedBurgerBarBottom = posed('span')({
+  closed: {
+    y: 0,
+    rotate: 0,
+  },
+  open: {
+    y: -8,
+    rotate: -45,
+    transition: {
+      y: {
+        delay: 100,
+      },
+      rotate: {
+        delay: 350,
+        type: 'spring',
+        stiffness: 400,
+        damping: 18,
+      },
+    },
+  },
+})
