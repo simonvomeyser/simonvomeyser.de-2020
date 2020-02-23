665299daf72f1fb4da490f64e301d0532f8f9937
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-02-23 15:31:53 +0100

    Fix ssr issue

--- src/components/UnsupportedBrowserRedirect.js
+++ src/components/UnsupportedBrowserRedirect.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import * as Bowser from 'bowser'
 import { isSearchEngineBot } from '../util/isSearchEngineBot'
 
 export default function UnsupportedBrowserRedirect() {
-  const browser = Bowser.getParser(window.navigator.userAgent)
-  const isValidBrowser = browser.satisfies({
-    // declare browsers per OS
-    windows: {
-      'internet explorer': '<11',
-    },
-    // per platform (mobile, desktop or tablet)
-    mobile: {
+  if (typeof window !== undefined) {
+    const Bowser = require('bowser')
+    const browser = Bowser.getParser(window.navigator.userAgent)
+    const isValidBrowser = browser.satisfies({
+      // declare browsers per OS
+      windows: {
+        'internet explorer': '<11',
+      },
+      // per platform (mobile, desktop or tablet)
+      mobile: {
+        safari: '>=9',
+        'android browser': '>4.4',
+      },
+
+      // or in general
+      chrome: '>=29',
+      firefox: '>28',
+      opera: '>=17',
       safari: '>=9',
-      'android browser': '>4.4',
-    },
+      edge: '>=12',
+    })
 
-    // or in general
-    chrome: '>=29',
-    firefox: '>28',
-    opera: '>=17',
-    safari: '>=9',
-    edge: '>=12',
-  })
-
-  if (typeof window !== undefined && !isSearchEngineBot() && !isValidBrowser) {
-    window.location.href = 'https://legacy.simonvomeyser.de'
+    if (!isSearchEngineBot() && !isValidBrowser) {
+      window.location.href = 'https://legacy.simonvomeyser.de'
+    }
   }
   return null
 }
