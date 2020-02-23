1c106371059a1ced7f7733ee4edd806d822f816b
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-02-23 16:46:38 +0100

    Attempt to fix ssr issue closue based

--- src/components/UnsupportedBrowserRedirect.js
+++ src/components/UnsupportedBrowserRedirect.js
@@ -1,32 +1,10 @@
 import React from 'react'
 import { isSearchEngineBot } from '../util/isSearchEngineBot'
+import { isUnsupportedBrowser } from '../util/isUnsupportedBrowser'
 
 export default function UnsupportedBrowserRedirect() {
-  if (typeof window !== undefined) {
-    const Bowser = require('bowser')
-    const browser = Bowser.getParser(window.navigator.userAgent)
-    const isValidBrowser = browser.satisfies({
-      // declare browsers per OS
-      windows: {
-        'internet explorer': '<11',
-      },
-      // per platform (mobile, desktop or tablet)
-      mobile: {
-        safari: '>=9',
-        'android browser': '>4.4',
-      },
-
-      // or in general
-      chrome: '>=29',
-      firefox: '>28',
-      opera: '>=17',
-      safari: '>=9',
-      edge: '>=12',
-    })
-
-    if (!isSearchEngineBot() && !isValidBrowser) {
-      window.location.href = 'https://legacy.simonvomeyser.de'
-    }
+  if (!isSearchEngineBot() && isUnsupportedBrowser()) {
+    window.location.href = 'https://legacy.simonvomeyser.de'
   }
   return null
 }
---
+++ src/util/isUnsupportedBrowser.js
@@ -0,0 +1,26 @@
+export const isUnsupportedBrowser = () => {
+  if (typeof window === 'undefined') {
+    return false
+  }
+
+  const Bowser = require('bowser')
+  const browser = Bowser.getParser(window.navigator.userAgent)
+  return browser.satisfies({
+    // declare browsers per OS
+    windows: {
+      'internet explorer': '<11',
+    },
+    // per platform (mobile, desktop or tablet)
+    mobile: {
+      safari: '>=9',
+      'android browser': '>4.4',
+    },
+
+    // or in general
+    chrome: '>=29',
+    firefox: '>28',
+    opera: '>=17',
+    safari: '>=9',
+    edge: '>=12',
+  })
+}
