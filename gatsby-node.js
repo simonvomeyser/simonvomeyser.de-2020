37df30a8be7b050b2ffa5acabca44c3e10232fb6
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-17 16:46:01 +0100

    Remove rendering of unnecessary pages to speed up

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,4 +2,4 @@ const createPages = require('./src/gatsby-node/createPages')
 const onCreatePage = require('./src/gatsby-node/onCreatePage')
 
 exports.onCreatePage = onCreatePage
-exports.createPages = createPages
+// exports.createPages = createPages
