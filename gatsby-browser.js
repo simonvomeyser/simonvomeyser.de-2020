2ab2d9f273fce7455e57974b5b8b19279c8338bf
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-18 19:00:52 +0100

    Move fade to dedicated wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ConsoleJoke from './src/components/ConsoleJoke'
+import InitialPageFade from './src/components/InitialPageFade';
 
 export const onClientEntry = () => {
   localStorage.setItem('startedAt', Date.now())
@@ -9,7 +10,9 @@ export const onClientEntry = () => {
 export const wrapRootElement = ({ element, props }) => {
   return (
     <>
-      {element}
+      <InitialPageFade>
+        {element}
+      </InitialPageFade>
       <ConsoleJoke />
     </>
   )
---
+++ src/components/InitialPageFade.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import posed, { PoseGroup } from 'react-pose'
+import styled from 'styled-components'
+
+export default ({ children }) => (
+   <PosedWrapper initialPose="pageInvisible" pose="pageFadedIn">
+      {children}
+   </PosedWrapper>
+)
+
+const StyledWrapper = styled.div`
+   opacity: 0;
+`;
+
+const PosedWrapper = posed(StyledWrapper)({
+   pageFadedIn: {
+      opacity: 1,
+      delay: 200
+   },
+   pageInvisble: {
+      opacity: 0,
+   }
+})
