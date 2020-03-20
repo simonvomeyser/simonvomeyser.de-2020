7dc511a91cb9625cd666d4aefe3c65285be4ea90
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-20 17:22:39 +0100

    Create on wrap with language context to use later

--- src/gatsby-node/wrapRootElement.js
+++ src/gatsby-node/wrapRootElement.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ConsoleJoke from '../components/ConsoleJoke'
 import InitialPageFade from '../components/InitialPageFade';
+import LanguageContext from '../i18n/LanguageContext'
 
 export default ({ element, props }) => {
   return (
     <>
-      <InitialPageFade>
-        {element}
-      </InitialPageFade>
-      <ConsoleJoke />
+      <LanguageContext.Provider value="en">
+        <InitialPageFade>
+          {element}
+        </InitialPageFade>
+        <ConsoleJoke />
+      </LanguageContext.Provider>
     </>
   )
 }
