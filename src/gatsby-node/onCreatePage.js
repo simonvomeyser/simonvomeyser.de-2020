4aa351f4d633b379af0cff4a7a3a90ec472b5644
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-22 18:01:06 +0100

    Extract function, add todo for creating page with translated URL

--- src/gatsby-node/onCreatePage.js
+++ src/gatsby-node/onCreatePage.js
@@ -2,42 +2,54 @@ const path = require('path');
 const { languages } = require('../i18n/locales')
 
 module.exports = ({ page, actions }) => {
-    const { createPage, deletePage } = actions
-  
-    if (page.path.includes('404')) {
-      return Promise.resolve()
+
+  if (page.path.includes('404')) {
+    return Promise.resolve()
+  }
+
+  return createTwoPagesWithTranslatedURL(page, actions);
+
+}
+
+const createTwoPagesWithTranslatedURL = (page, actions) => {
+  const { createPage, deletePage } = actions
+
+  // todo
+  // delte actual page
+  // create for all languages a page with TRANSLATED url
+  // Wrap this page into the SetLanguage HOC with the currently active page
+
+  return new Promise(resolve => {
+    const redirect = path.resolve('src/i18n/redirect.js')
+    const redirectPage = {
+      ...page,
+      component: redirect,
+      context: {
+        languages,
+        locale: '',
+        routed: false,
+        redirectPage: page.path,
+      },
     }
-  
-    return new Promise(resolve => {
-      const redirect = path.resolve('src/i18n/redirect.js')
-      const redirectPage = {
+    deletePage(page)
+    createPage(redirectPage)
+
+    languages.forEach(({ value }) => {
+      const localePage = {
         ...page,
-        component: redirect,
+        originalPath: page.path,
+        path: `/${value}${page.path}`,
         context: {
           languages,
-          locale: '',
-          routed: false,
-          redirectPage: page.path,
+          locale: value,
+          routed: true,
+          originalPath: page.path,
         },
       }
-      deletePage(page)
-      createPage(redirectPage)
-  
-      languages.forEach(({ value }) => {
-        const localePage = {
-          ...page,
-          originalPath: page.path,
-          path: `/${value}${page.path}`,
-          context: {
-            languages,
-            locale: value,
-            routed: true,
-            originalPath: page.path,
-          },
-        }
-        createPage(localePage)
-      })
-  
-      resolve()
+      createPage(localePage)
     })
-  }
+
+    resolve()
+  })
+
+}
