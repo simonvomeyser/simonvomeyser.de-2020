eb13c11b59e0352bb3d09c0b8b8c35f1e3dd07bc
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-23 12:34:30 +0100

    Transform to functional component

--- src/components/ChangeTitle.js
+++ src/components/ChangeTitle.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 import { injectIntl } from 'react-intl'
 import Helmet from 'react-helmet'
 
-class ChangeTitle extends Component {
-  render() {
-    const { intl, additionalText } = this.props
-    const finalAdditionalText = this.props.translate
-      ? intl.formatMessage({ id: additionalText })
-      : additionalText
-    return (
-      <Helmet
-        title={
-          intl.formatMessage({ id: 'title' }) + ' - ' + finalAdditionalText
-        }
-      />
-    )
-  }
+const ChangeTitle = ({ intl, additionalText, translate }) => {
+  const finalAdditionalText = translate
+    ? intl.formatMessage({ id: additionalText })
+    : additionalText
+  return (
+    <Helmet
+      title={
+        intl.formatMessage({ id: 'title' }) + ' - ' + finalAdditionalText
+      }
+    />
+  )
 }
 
 export default injectIntl(ChangeTitle)
