1b5daa5a5e7cc620288cf2bfccea9107748bc049
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-24 10:37:42 +0100

    Remove page creation for now
    This commit can be referenced later if projects need single pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,3 @@
-const createPages = require('./src/gatsby-node/createPages')
 const onCreatePage = require('./src/gatsby-node/onCreatePage')
 
 exports.onCreatePage = onCreatePage
-// exports.createPages = createPages
