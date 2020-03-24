16970fccd303982e5b9905b5e1ffb2a9d83067a9
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-24 13:53:14 +0100

    Add is frontpage flag to all pages

--- src/gatsby-node/pageContext.js
+++ src/gatsby-node/pageContext.js
@@ -3,6 +3,7 @@ import React from 'react'
 const pageContext = React.createContext()
 
 export function PageContextProvider({ pageData, children }) {
+    pageData.isFrontpage = pageData.path === '/';
     return (
         <pageContext.Provider value={pageData}>
             {children}
