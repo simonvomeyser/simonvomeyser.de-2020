bd9cb20465c7869b77d28a76a3b5761b6d745ec9
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-03-30 15:53:17 +0200

    Revert "Attempt to fix display of unsupported page"
    This reverts commit a8c876dc8834b1ea228d998531eb9d28ccd89182.

--- src/components/OnlySupportedBrowsers.js
+++ src/components/OnlySupportedBrowsers.js
@@ -15,9 +15,8 @@ import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 export default function OnlySupportedBrowsers({ children }) {
   if (!isSearchEngineBot() && !isSupportedBrowser()) {
     return <UnsupportedPage />
-  } else {
-    return <>{children}</>
   }
+  return (<>{children}</>)
 }
 
 const UnsupportedPage = () => {
