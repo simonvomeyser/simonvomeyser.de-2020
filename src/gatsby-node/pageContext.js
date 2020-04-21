aa525e58c85f64932c23595294d86cbd08cb81dd
Simon vom Eyser <simon.vom.eyser@gmail.com>
2020-04-21 12:11:59 +0200

    Add key of page to page context

--- src/gatsby-node/pageContext.js
+++ src/gatsby-node/pageContext.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+const { getPathKey } = require('../i18n/translatedPathHelper')
+
 const pageContext = React.createContext()
 
 export function PageContextProvider({ pageData, children }) {
     pageData.isFrontpage = pageData.path === '/';
+    pageData.key = getPathKey(pageData.path) || 'index';
+
     return (
         <pageContext.Provider value={pageData}>
             {children}
